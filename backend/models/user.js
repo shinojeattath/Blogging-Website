46161f10961573d051507f5f7734770b49ea2225
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2024-06-30 01:24:29 +0530

    added username

--- backend/models/user.js
+++ backend/models/user.js
@@ -9,6 +9,10 @@ const schema = mongoose.Schema({
         type: String,
         required: true
     },
+    username:{
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
